340099f93b9c43eb7542da9edb06ca1dd5bf1d6a
Islahihyaa <islah.ihya@gmail.com>
2024-04-21 21:10:39 +0700

    RPL450102-32 fix:authentication login and register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,64 +6,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <!-- <link rel="stylesheet" href="style.css"> -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg ">
-            <div class="container-fluid px-5 justify-content-end">
-                <div class="d-flex">
-                    <img src="images/image 3.png" alt="">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+<div class="">
+  <div class="row align-items-center">
+    <div class="col-md-6 text-center">
+      <img src="images/imageAuth.png" alt="Image on Authentication">
+      <h1 class="text-center">Tingkatkan Kebersihan <br> Lingkunganmu di WhizCycle</h1>
+    </div>
+    <div class="col-md-6 bg-custom min-height-container d-flex align-items-center justify-content-center">
+      <div class="col-md-11">
+        <div class="card-text">
+          <h4 class="card-title fs-1 mb-2 text-white">Kirim Sampahmu Sekarang!</h4>
+          <p class="card-text fs-5 mb-5">Sudah Punya Akun? 
+            <a href="/" class="text-white text-decoration-none"> Masuk </a>
+          </p>
+        </div>
+          @if ($errors->any())
+              <div class="alert alert-danger">
+                  @foreach ($errors->all() as $error)
+                      <p>{{ $error }}</p>
+                  @endforeach
+              </div>
+          @endif
+          <form method="post">
+                @csrf
+                <div class="form-group">
+                    <input id="my-input" class="form-control input-lg mb-4" type="text" name="name" placeholder="Nama Lengkap" required>
+                </div>
+                <div class="form-group ">
+                    <input id="my-input" class="form-control input-lg mb-4" type="text" name="address" placeholder="Alamat" required>
+                </div>
+                <div class="form-group ">
+                    <input id="my-input" class="form-control input-lg mb-4" type="number" name="phoneNo" placeholder="No. Telp" required>
+                </div>
+                <div class="form-group ">
+                    <input id="my-input" class="form-control input-lg mb-4" type="password" name="password" placeholder="Password" required>
+                </div>
+                <div class="text-end">
+                    <button type="submit" name="register" id=""class="btn btn-white input-lg">
+                        Daftar
                     </button>
                 </div>
-            </div>
-        </nav>
-    </header>
-
-    <div class="container py-5">
-        <div class="row rows-cols-2 justify-content-between">
-            <div class="col-6">
-                <img src="images/amico.png" alt="">
-            </div>
-            <div class="col-4">
-                <h3>Daftar</h3>
-                <form action="" method="post">
-                    @csrf
-                    <div class="form-group mt-3">
-                        <label for="my-input my-2">Nama Lengkap</label>
-                        <input id="my-input" class="form-control" type="text" name="name" required>
-                    </div>
-                    <div class="form-group mt-3">
-                        <label for="my-input my-2">Alamat</label>
-                        <input id="my-input" class="form-control" type="text" name="address" required>
-                    </div>
-                    <div class="form-group mt-3">
-                        <label for="my-input my-2">No. Hp</label>
-                        <input id="my-input" class="form-control" type="number" name="phoneNo" required>
-                    </div>
-                    <div class="form-group mt-3">
-                        <label for="my-input my-2">Password</label>
-                        <input id="my-input" class="form-control" type="password" name="password" required>
-                    </div>
-                    <div class="d-grid gap-2 mt-3">
-                        <button type="submit" name="register" id="" class="btn btn-success py-2">
-                            Continue
-                        </button>
-                    </div>
-
-                </form>
-                <div class="text-center mt-4">
-                    Already have account ? <a href="/" class="text-success text-decoration-none">Login Here</a>
-                </div>
-
-            </div>
-
-
-        </div>
+            </form>
+      </div>
     </div>
+  </div>
+</div>
 </body>
 
 </html>
--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -1,1 +1,39 @@
-index
+@extends('layout')
+
+@section('title', 'WhizCycle')
+
+@section('content')
+
+    <main id="main" class="main">
+        <!-- Page Content  -->
+        <div class="pagetitle">
+            <h1>Beranda</h1>
+        </div>
+
+        <section class="section">
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                        <div class="row row-cos-1">
+                           <div class="col-md-6">
+                                <h1>Jadwalkan Pengambilan Sampah Anda Sekarang! Dengan Mudah dan Efisien!</h1>
+                                <p>Aplikasi WhizCycle adalah solusi untuk menyelesaikan masalah sosial tentang kebersihan lingkungan.</p>
+                                <p>Mulai sekarang, atur jadwal pengambilan sampah Anda dengan mudah! Pilih tanggal dan tentukan waktu  pengumpulan sampah Anda. Kami siap membantu Anda menjaga lingkungan bersih dan memberikan layanan yang praktis dan efisien. Ayo beraksi sekarang!</p>
+                                <div class="text-start">
+                                    <a href ="order" name="order" class="btn btn-success btn-lg">Pesan Ojol</a>
+                                </div>
+                           </div>
+                            <div class="col-md-6">
+                                <img src="images/Driver.png" alt="image" width="300" height="200">
+                            </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        </div>
+    </div>
+
+@endsection
