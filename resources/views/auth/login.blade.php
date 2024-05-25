b72fed15f25a31aad2c28f6754b9c6f91a10a1f8
haikalzulhajj <haikalzulhajj@gmail.com>
2024-05-26 00:03:25 +0700

    Initial Commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,44 +19,40 @@
 </head>
 
 <body>
-  <div class="">
-    <div class="row align-items-center">
-      <div class="col-md-6 text-center">
-      <img src="images/imageAuth.jpg" alt="Image on Authentication" style="width: 50%;">
-        <h1 class="text-center">Your Journey <br> to Brighter Tomorrows</h1>
-      </div>
-      <div class="col-md-6 custom-bg-purple min-height-container d-flex align-items-center justify-content-center">
-        <div class="col-md-11">
-          <div class="card-text">
-            @if(Session::has('message'))
+    <div class="container centered-container">
+        <div class="text-center mb-4">
+            <img src="images/imageAuth.jpg" alt="Image on Authentication" class="img-fluid custom-img-size">
+            <h1 class="mt-3">Your Journey <br> to Brighter Tomorrows</h1>
+        </div>
+        <div class="card custom-bg-purple p-4">
+            <div class="card-text">
+                @if(Session::has('message'))
                     <div class="alert alert-danger"> {{ Session::get('message') }}</div>
                 @endif
                 @if(Session::has('status'))
                     <div class="alert alert-success"> {{ Session::get('status') }}</div>
                 @endif
-            <h4 class="card-title fs-1 mb-2 text-white">Sign in</h4>
-            <p class="card-text fs-5 mb-5">Don't have an account? 
-              <a href="register" class="text-white text-decoration-none"> Sign up </a>
-            </p>
-          </div>
-          <form method="POST">
-            @csrf
-              <div class="form-group">
-                <label for="username" class="text-white bold-label">USERNAME</label>
-                <input id="username" class="form-control input-lg mb-4" type="text" name="name" placeholder="Username" required>
-              </div>
-              <div class="form-group">
-                <label for="password" class="text-white bold-label">PASSWORD</label>
-                <input id="password" class="form-control input-lg mb-5" type="password" name="password" placeholder="Password" required>
-              </div>
-              <div class="text-end">
-                <button type="submit" name="login" class="btn btn-white input-lg">Sign in</button>
-              </div>
-          </form>
+                <h4 class="card-title fs-2 mb-2 text-white">Sign in</h4>
+                <p class="card-text fs-5 mb-4">Don't have an account? 
+                    <a href="register" class="text-white text-decoration-none"> Sign up </a>
+                </p>
+            </div>
+            <form method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="username" class="text-white bold-label">USERNAME</label>
+                    <input id="username" class="form-control input-lg mb-3" type="text" name="name" placeholder="Username" required>
+                </div>
+                <div class="form-group">
+                    <label for="password" class="text-white bold-label">PASSWORD</label>
+                    <input id="password" class="form-control input-lg mb-4" type="password" name="password" placeholder="Password" required>
+                </div>
+                <div class="text-center">
+                    <button type="submit" name="login" class="btn btn-white input-lg">Sign in</button>
+                </div>
+            </form>
         </div>
-      </div>
     </div>
-  </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
